04672aa10909e4906cedeb416f8645cd2d573235
Brojananda <bropaul@umich.edu>
2024-11-17 21:59:13 -0500

    A start for main.py

--- filter_applier/main.py
+++ filter_applier/main.py
@@ -29,3 +29,31 @@ Requirements:
 1. Stores a copy of original training set and test set to compare accuracy later on
 2. Need a way to test original data set with modified one without bias (can't use same filters on test set because modified one will obviously score higher)
 """
+
+import time
+import sys
+import random
+
+# Fetch the time
+seconds = time.time()
+
+# Set random seed using time
+
+# drone_pics folder
+
+#Check if exist
+if drone_pics is None :
+    print ("Error opening image")
+    sys.exit(-1)
+
+
+# For each image in the dataset, choose which ones to modify and apply filters
+# Generate numbers 1-100, if numbers 1-20, modify the image
+random_num = random.randint(1,100)
+if random_num <= 20 :
+    #Modify
+# Generate numbers 1-4 to choose which filter to apply/Randomly determine the order
+# of the filters and apply them repeatedly in that order.
+# Code: 1: Bloom. 2: Blur. 3: Chromatic Aberration. 4: Film Grain.
+    
+# Write to modified_directory as we go through all the images (within loop)
